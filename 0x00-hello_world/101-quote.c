967b74452023393a915ac4f832ebe828ff3f331b
Basil Baraka Wandukwa <wandukwabaraka@gmail.com>
2023-02-16 14:09:14 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Ac program that prints a line to the standard error
+ * Return 1 (SUcess)
+ */
+int main(101-quote.c)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" stdout);
+	return (1);
+}
