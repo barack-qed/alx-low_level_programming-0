0f870f542b19258d434262ad99e5604ea8ecf69c
Basil Baraka Wandukwa <wandukwabaraka@gmail.com>
2023-02-16 13:23:00 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A programme that prints a lione using the printf functio
+ * return: 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n")
+		return (0);
+}
