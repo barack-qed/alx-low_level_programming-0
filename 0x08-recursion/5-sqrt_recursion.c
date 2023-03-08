40b5942f2ca8c6cfc11675c8adf884e5d97dd589
Basil Baraka Wandukwa <wandukwabaraka@gmail.com>
2023-03-07 23:04:34 -0800

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -4,7 +4,7 @@
 int _sqrt(int n, int i);
 
 /**
- * _sqrt_recursion - Returns the natural square root of a number 
+ * _sqrt_recursion - Returns the natural square root of a number
  * @n: number to calculate the natural square root
  *
  * Return: the natural square root
