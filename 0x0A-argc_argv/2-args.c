f334dc77bf69be465b68141a1eb1b992e26de86d
Basil Baraka Wandukwa <wandukwabaraka@gmail.com>
2023-03-10 03:19:07 -0800

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * mian - prints all the args content
+ * @argc: argument count
+ * argv: argument vector
+ *
+ * Return: Always zero
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
